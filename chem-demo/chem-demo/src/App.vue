a010295d42f1ff82f3163cd869a20b7598498ad1
pada <598574029@qq.com>
2024-01-15 20:49:12 +0800

    前端原始代码上传

---
+++ chem-demo/chem-demo/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <router-view/>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #f0f2f5;
+  height: 100%;
+}
+
+nav {
+  padding: 0px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ chem-demo/chem-demo/vue.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  lintOnSave:false,
+  devServer: {
+    proxy: 'http://192.168.50.59:8888/'
+  }
+
+})
+
+
